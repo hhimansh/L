c12542076d76d1a990587b0e479224bd9534a349
hhimansh <himanshu.aggarwal116@gmail.com>
2022-10-07 17:04:05 -0400

    2add.js file added

---
+++ 2add.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]} 
+ */
+var twoSum = function (nums, target) {
+  b = 0;
+  a = 1;
+  i = nums[a];
+  j = nums[b];
+  result = [];
+  while (b <= nums.length) {
+    i = nums[a];
+    j = nums[b];
+    while (a <= nums.length) {
+      if (i + j == target) {
+        document.getElementById("result").innerHTML += `${i+j}`;
+        result[0] = b;
+        result[1] = a;
+        return result;
+      } else {
+        a++;
+      }
+    }
+    b++;
+    a = b + 1;
+  }
+  return result;
+};
